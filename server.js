afad286b321cfac852accd384dbb30adc7c869df
git config --global user.name <parvathinathanbala@gmail.com>
2023-01-07 14:37:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+const port = 5092;  
+const database = require('mysql');
+//  connect to database
+let connection = database.createConnection({
+    host:'localhost',
+    user:'root',
+    password:"",
+    database:''
+})
+app.set("view engine","ejs");
+app.get('/index',(req,res)=>{
+res.render("index");
+})
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+app.use(bodyParser.json());
+
+
+app.use(express.static("public"));
+// listen port
+app.listen(port,()=>console.log("listening server",port));  
