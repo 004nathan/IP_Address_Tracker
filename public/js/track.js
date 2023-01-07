33d99ba76be876cb5c9ff5dae4d3bebafe1730cf
git config --global user.name <parvathinathanbala@gmail.com>
2023-01-07 16:06:25 +0530

    commit

--- public/js/track.js
+++ public/js/track.js
@@ -1,15 +1,15 @@
 
 const ipTracker = function(){
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_q6vTjgcPP0sVPqJII09xeQAeD1u5J&ipAddress=8.8.8.8`).then(function (response){
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_q6vTjgcPP0sVPqJII09xeQAeD1u5J&ipAddress=8.8.8.8`).then(function (response){
       console.log(response);
       console.log(response.url);
       return response.json();
     }).then(function(response){
         console.log(response)
         console.log(response.location.region);
-        fetch(`https://restcountries.com/v3.1/name/${response.location.region}`).then(function(response){
-            console.log(response.json())
-        })
+        console.log(response.location.lat);
+        console.log(response.location.lng);
+        
     }).catch(err =>{
         console.log(`ip address not found${err}`)
       })
