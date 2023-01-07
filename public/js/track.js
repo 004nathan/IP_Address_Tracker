de1ffac0d19be6d3493e82215f70169709571f3e
git config --global user.name <parvathinathanbala@gmail.com>
2023-01-07 16:03:11 +0530

    get lattitude

--- public/js/track.js
+++ public/js/track.js
@@ -1,8 +1,15 @@
 
 const ipTracker = function(){
-    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_q6vTjgcPP0sVPqJII09xeQAeD1u5J&ipAddress=172.17.61.92`).then(function (response){
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_q6vTjgcPP0sVPqJII09xeQAeD1u5J&ipAddress=8.8.8.8`).then(function (response){
       console.log(response);
+      console.log(response.url);
       return response.json();
+    }).then(function(response){
+        console.log(response)
+        console.log(response.location.region);
+        fetch(`https://restcountries.com/v3.1/name/${response.location.region}`).then(function(response){
+            console.log(response.json())
+        })
     }).catch(err =>{
         console.log(`ip address not found${err}`)
       })
